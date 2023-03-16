3eb064c3d0431e0865c8e4fda1ef260cc4b87632
Andrey0573 <95186528+Andrey0573@users.noreply.github.com>
2023-03-16 15:49:45 +0400

    color output version

--- Math/math.c
+++ Math/math.c
@@ -1,56 +1,24 @@
-#include <stdio.h>      // printf(), scanf()
-#include <stdlib.h>     // rand(), srand()
-#include <time.h>       // time()
+/*
+#include <stdio.h>
+  #include <stdlib.h>
 
-int main () {
-	
-system("clear");
-time_t t;
-                   /* Intializes random number generator */
-                   srand((unsigned) time(&t));
-				   
-while (1) {
+  int main () {
+     int i, n;
+        time_t t;
 
-int a, b, c, d;
-                   
-a = rand() % 11;
-b = rand() % 11;
-//                 printf("\n a = %d    b = %d    c = ?\n", a, b);
-                   
-   if (  a >= b )
-        {         
-//      printf("\n a - b = c \n");
-        printf("\n %d - %d = ?\n\n", a, b);
-        scanf("%2d", &d);
-           if ( d==a-b )
-            {
-            system("clear");
-            printf("\n OK! \n\n\n");
-            }
-           else
-              {
-              system("clear");
-              printf("\n ERROR! \n\n\n");
-              }
-        }
+              n = 5;
 
-   else 
-        {
-//      printf("\n b - a = c \n");
-        printf("\n %d - %d = ? \n\n", b, a);
-        scanf("%2d", &d);
-           if ( d==b-a )
-            {
-            system("clear");
-            printf("\n OK! \n\n\n");
-            }
-           else
-              {
-              system("clear");
-              printf("\n ERROR! \n\n\n");
-              }
+ */
+                    /* Intializes random number generator */
+/*
+srand((unsigned) time(&t));
+*/
+      /* Print 5 random numbers from 0 to 49 */
+/*      for( i = 0 ; i < n ; i++ ) {
+                    printf("%d\n", rand() % 50);
+                       }
 
-        }
-   }              
-return(0);
-}
+   return(0);
+   }
+
+*/
