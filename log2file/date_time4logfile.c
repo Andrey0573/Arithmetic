3e47fa737b909fcbb87c866cb0ad6a6da6c6a044
root <root@linux-d5ki.site>
2024-06-23 05:52:17 +0400

    added folder log2file

---
+++ log2file/date_time4logfile.c
@@ -0,0 +1,34 @@
+#include <stdio.h>      /*  // printf(), scanf()  */
+#include <stdlib.h>     /*  // rand(), srand()    */
+#include <time.h>       /*  // time()             */
+
+time_t unix_time;
+char date_time[20];
+
+
+void date_time4log()	{
+//	time_t unix_time;
+//    	char* time_string;
+	/* Obtain current time. */
+//	unix_time = time(NULL);
+	time (&unix_time);
+	strftime(date_time, 20, "%Y-%m-%d %H:%M:%S", localtime(&unix_time)); 
+        
+}
+
+int main () {
+
+date_time4log();
+
+   FILE * fp;
+   fp = fopen ("logfile.log", "a");
+   fprintf(fp, "%s loginfo  \n", date_time);
+   fclose(fp);
+ 
+//   printf(" %s The current date is\n", date_time);
+ 
+   return(0);
+
+}
+
+
